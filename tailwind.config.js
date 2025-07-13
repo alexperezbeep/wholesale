a9ca526347d37b6a82d3712ae39d7b0119378571
alexperezbeep <alexandergperezz@gmail.com>
2025-07-13 03:01:13 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class', // <--- ADD THIS
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
