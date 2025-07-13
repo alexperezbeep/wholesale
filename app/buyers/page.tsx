68b5f8bdea959eecbf58bfc09739f7bb424cdb22
alexperezbeep <alexandergperezz@gmail.com>
2025-07-13 00:28:37 -0400

    Create page.tsx

---
+++ app/buyers/page.tsx
@@ -0,0 +1,8 @@
+export default function BuyersPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">Buyers</h1>
+      <p className="mt-4 text-gray-700">Details for investor buyers will go here.</p>
+    </main>
+  );
+}
