23e84e1660463eb5df28f3d73e9ef4a5e6f2aa4e
alexperezbeep <alexandergperezz@gmail.com>
2025-07-14 01:48:51 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
@@ -13,5 +14,3 @@ const config: Config = {
     require('@tailwindcss/typography'),
   ],
 };
-
-export default config;
